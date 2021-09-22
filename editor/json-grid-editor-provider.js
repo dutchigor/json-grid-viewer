16115050087942ed431b576d37dabefc63597ca8
igor <igor@flarehub.io>
2021-09-22 21:51:32 +0300

    Make view type more specific

--- editor/json-grid-editor-provider.js
+++ editor/json-grid-editor-provider.js
@@ -1,23 +1,23 @@
-const vscode = require( 'vscode' )
+const vscode = require('vscode')
 const { JsonGridViewer } = require('./json-grid-viewer')
 
 class JsonGridEditorProvider {
 
-	constructor( context ) {
-    this.context = context
+	constructor(context) {
+		this.context = context
 	}
-	
+
 	static register(context) {
-		const viewType = 'jsonGridViewer'
+		const viewType = 'jsonGridViewer.json'
 		const providerRegistration = vscode.window.registerCustomEditorProvider(
-      viewType, new JsonGridEditorProvider( context )
-    )
+			viewType, new JsonGridEditorProvider(context)
+		)
 		return providerRegistration
 	}
 
-  async resolveCustomTextEditor( document, webviewPanel, _token ) {
+	async resolveCustomTextEditor(document, webviewPanel, _token) {
 		// Initialise viewer
-		const jsonGridViewer = new JsonGridViewer( document, webviewPanel, this.context )
+		const jsonGridViewer = new JsonGridViewer(document, webviewPanel, this.context)
 
 		// Make sure we clean up when our editor is closed.
 		webviewPanel.onDidDispose(() => {
